331b5e73fa7f22634be18cfac36672de9edcd359
Antônio Marcos <antoniomarcos.souza2002@gmail.com>
2025-02-03 20:11:39 -0300

    feature: entidade e repository do colaborador

---
+++ src/main/java/ufjf/dcc061/payroll_pro/ColaboradorRepository.java
@@ -0,0 +1,7 @@
+package ufjf.dcc061.payroll_pro;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ColaboradorRepository extends JpaRepository<Colaborador, String> {
+
+}
