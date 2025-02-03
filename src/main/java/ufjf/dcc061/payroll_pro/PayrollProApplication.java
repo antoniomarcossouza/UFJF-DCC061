e92ab443a1dfec945aa1947e8856e9f41eac4fe1
Antônio Marcos <antoniomarcos.souza2002@gmail.com>
2025-02-03 19:37:34 -0300

    chore: inicializando projeto

---
+++ src/main/java/ufjf/dcc061/payroll_pro/PayrollProApplication.java
@@ -0,0 +1,13 @@
+package ufjf.dcc061.payroll_pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayrollProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ufjf/dcc061/payroll_pro/PayrollProApplicationTests.java
@@ -0,0 +1,13 @@
+package ufjf.dcc061.payroll_pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
