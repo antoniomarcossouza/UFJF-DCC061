962ee5d31457602716c56a23119fdf8e7b04899f
Antônio Marcos <antoniomarcos.souza2002@gmail.com>
2025-02-03 20:48:16 -0300

    feature: colaborador controller com docker

--- src/main/java/ufjf/dcc061/payroll_pro/PayrollProApplication.java
+++ src/main/java/ufjf/dcc061/payroll_pro/PayrollProApplication.java
@@ -1,13 +1,25 @@
 package ufjf.dcc061.payroll_pro;
 
+import java.util.Date;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PayrollProApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PayrollProApplication.class, args);
+
+	}
+
+	@Bean
+	public CommandLineRunner demo(ColaboradorRepository colaboradorRepository) {
+		return (args) -> {
+			colaboradorRepository
+					.save(new Colaborador("202065245C", "Antônio", new Date(), null, "Ativo", 10.0, "Desenvolvedor"));
+		};
 	}
 
 }
